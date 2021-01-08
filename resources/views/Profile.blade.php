9ca891109bac10f7980485fb8895307c0b8a59e1
Vinuri Prabodhya <50774562+vinuPAbeywarna@users.noreply.github.com>
2021-01-08 19:53:23 +0530

    Update Profile.blade.php

--- resources/views/Profile.blade.php
+++ resources/views/Profile.blade.php
@@ -143,7 +143,7 @@
                                                 <tr>
                                                     <td>{{$Report->HarvestType}} | {{$Report->updated_at}}</td>
                                                     <td>
-                                                        <form method="post" action="/updateUser">
+                                                        <form action="" method="post" action="/updateUser">
                                                         <div class="btn-group btn-group-sm" role="group">
                                                             <button class="btn btn-primary" type="button">Edit</button>
                                                             <button class="btn btn-danger" type="button">Delete</button>
