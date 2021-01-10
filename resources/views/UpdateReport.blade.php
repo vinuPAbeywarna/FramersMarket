7b0ddee3d0db9fce45ff42bdb44746295e9c2c28
Vinuri Prabodhya <50774562+vinuPAbeywarna@users.noreply.github.com>
2021-01-11 00:31:43 +0530

    Update UpdateReport.blade.php

--- resources/views/UpdateReport.blade.php
+++ resources/views/UpdateReport.blade.php
@@ -78,7 +78,7 @@
                                         <option @if($Report->District=='Jaffna') selected @endif value="Kandy">Kandy</option>
                                         <option @if($Report->District=='Jaffna') selected @endif value="Kegalle">Kegalle</option>
                                         <option @if($Report->District=='Jaffna') selected @endif value="Kilinochchi">Kilinochchi</option>
-                                        <option value="kurunegala">kurunegala</option>
+                                        <option @if($Report->District=='Jaffna') selected @endif value="kurunegala">kurunegala</option>
 
                                         <option value="Matara">Matara</option>
                                         <option value="Matale">Matale</option>
