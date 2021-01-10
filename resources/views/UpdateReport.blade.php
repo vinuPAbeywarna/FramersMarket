3737b816c646dd45315772af8a5fe97a1f54f1f1
Vinuri Prabodhya <50774562+vinuPAbeywarna@users.noreply.github.com>
2021-01-11 00:37:13 +0530

    Update UpdateReport.blade.php

--- resources/views/UpdateReport.blade.php
+++ resources/views/UpdateReport.blade.php
@@ -87,7 +87,7 @@
                                         <option @if($Report->District=='Nuwara Eliya') selected @endif value="Nuwara Eliya">Nuwara Eliya</option>
 
                                         <option @if($Report->District=='Putthalama ') selected @endif value="Putthalama">Putthalama</option>
-                                        <option @if($Report->District=='Rathnapura Eliya') selected @endif value="Rathnapura">Rathnapura</option>
+                                        <option @if($Report->District=='Rathnapura ') selected @endif value="Rathnapura">Rathnapura</option>
                                         <option @if($Report->District=='Trincomalee ') selected @endif value="Trincomalee">Trincomalee</option>
                                         <option @if($Report->District=='Vavniya ') selected @endif value="Vavniya">Vavniya</option>
                                         <option @if($Report->District=='Matale ') selected @endif value="Matale">Matale</option>
