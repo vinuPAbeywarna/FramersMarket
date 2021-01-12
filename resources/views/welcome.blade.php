eadbadf8aa2d1895021c1e1c4ec4190fa02da7bc
Vinuri Prabodhya <50774562+vinuPAbeywarna@users.noreply.github.com>
2021-01-13 01:11:12 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,7 +31,6 @@
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                            
                         @endif
                     @endauth
                 </div>
