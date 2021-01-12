1dadd787ad025a0213b28117ea02ce2c0330fba7
Vinuri Prabodhya <50774562+vinuPAbeywarna@users.noreply.github.com>
2021-01-12 22:38:12 +0530

    Update SignIn.blade.php

--- resources/views/SignIn.blade.php
+++ resources/views/SignIn.blade.php
@@ -13,7 +13,6 @@
 </head>
 
 <body class="bg-gradient-primary">
-
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-9 col-lg-12 col-xl-10">
